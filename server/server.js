431087e8f401761df8f1b3b13af19b5af49890b1
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2020-12-29 08:29:32 +0700

    C05.S01 - About API.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
+
+let aboutMessage = 'Issue Tracker API v1.0';
+
+// Apollo server setup.
+const typeDefs = `
+  type Query {
+    about: String!
+  }
+
+  type Mutation {
+    setAboutMessage(message: String!): String
+  }
+`;
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+  },
+  Mutation: {
+    setAboutMessage: (_, { message }) => (aboutMessage = message),
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.use(express.static('./public'));
 
