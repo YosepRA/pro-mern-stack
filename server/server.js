a084b9e39cea8ed5e8d148cf0702bc38a890c43b
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2020-12-29 09:03:42 +0700

    C05.S03 - List API.

--- server/server.js
+++ server/server.js
@@ -5,16 +5,45 @@ const fs = require('fs');
 const app = express();
 
 let aboutMessage = 'Issue Tracker API v1.0';
+const issuesDB = [
+  {
+    id: 1,
+    status: 'New',
+    owner: 'Ravan',
+    effort: 5,
+    created: new Date('2019-01-15'),
+    due: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    effort: 14,
+    created: new Date('2019-01-16'),
+    due: new Date('2019-02-01'),
+    title: 'Missing bottom border on panel',
+  },
+];
 
 // Apollo server setup.
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
-    setAboutMessage: (_, { message }) => (aboutMessage = message),
+    setAboutMessage,
   },
 };
+
+function setAboutMessage(_, { message }) {
+  return (aboutMessage = message);
+}
+
+function issueList() {
+  return issuesDB;
+}
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
