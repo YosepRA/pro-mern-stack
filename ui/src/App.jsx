4753e94bbd307091e9333bb7f45d0787be5f118e
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2021-01-24 08:43:59 +0700

    C08.S02 - Frontend Modules and Webpack.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,42 +1,8 @@
 /* globals React ReactDOM PropTypes */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-no-undef */
-/* eslint-disable no-alert */
 
-const datePattern = /^\d\d\d\d-\d\d-\d\d/;
-
-function jsonDateReviver(key, value) {
-  if (datePattern.test(value)) return new Date(value);
-  return value;
-}
-
-async function graphQLFetch(query, variables = {}) {
-  try {
-    const response = await fetch(`${window.ENV.UI_API_ENDPOINT}/graphql`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query, variables }),
-    });
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDateReviver);
-
-    if (result.errors) {
-      const error = result.errors[0];
-
-      if (error.extensions.code === 'BAD_USER_INPUT') {
-        const details = error.extensions.errors.join('\n ');
-        alert(`${error.message}:\n ${details}`);
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`);
-      }
-    }
-
-    return result.data;
-  } catch (error) {
-    alert(`Error in sending data to server: ${error.message}`);
-    return null;
-  }
-}
+import graphQLFetch from './graphQLFetch.js';
 
 function IssueRow({ issue }) {
   return (
