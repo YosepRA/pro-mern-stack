60f3f865a898002c89ec4afc5205b3a476ddfa42
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2021-01-26 20:23:47 +0700

    C08.S05 - Hot Module Replacement.

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -9,6 +9,28 @@ const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000';
 const env = { UI_API_ENDPOINT };
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 
+// Webpack Hot Module Replacement configuration.
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+if (enableHMR && process.env.NODE_ENV !== 'production') {
+  console.log('Adding dev middleware and HMR...');
+  /* eslint-disable import/no-extraneous-dependencies */
+  /* eslint-disable global-require */
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+
+  // Modify config.
+  const config = require('./webpack.config');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  // Add middlewares.
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
 app.use(express.static('./public'));
 if (apiProxyTarget) app.use('/graphql', proxy({ target: apiProxyTarget }));
 
