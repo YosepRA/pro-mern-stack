f55804b0a2af5566c29594b291a2c2abd5c33ea7
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2021-02-12 15:55:58 +0700

    C11.S01 - Bootstrap Installation.

--- ui/src/IssueList.jsx
+++ ui/src/IssueList.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
 import { Route } from 'react-router-dom';
+import { Label } from 'react-bootstrap';
 
 import IssueFilter from './IssueFilter.jsx';
 import IssueTable from './IssueTable.jsx';
@@ -156,7 +157,9 @@ export default class IssueList extends React.Component {
 
     return (
       <React.Fragment>
-        <h1>Issue Tracker</h1>
+        <h1>
+          <Label>Issue Tracker</Label>
+        </h1>
         <IssueFilter />
         <hr />
         <IssueTable
