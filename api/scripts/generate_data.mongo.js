48aae1446c949e86b2d0b43a738e147d02b7fae9
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2021-03-04 17:23:02 +0700

    C13.S02 - MongoDB Aggregate.

---
+++ api/scripts/generate_data.mongo.js
@@ -0,0 +1,38 @@
+/* global db print */
+/* eslint-disable no-restricted-globals */
+
+const owners = ['Joe', 'Marvin', 'Garland', 'Jessica', 'Kalua'];
+const statuses = ['New', 'Assigned', 'Fixed', 'Closed'];
+
+const initialCount = db.issues.count();
+for (let i = 0; i < 100; i += 1) {
+  const randomCreatedDate =
+    new Date() - Math.floor(Math.random() * 60) * 1000 * 60 * 60 * 24;
+  const created = new Date(randomCreatedDate);
+  const randomDueDate =
+    new Date() - Math.floor(Math.random() * 60) * 1000 * 60 * 60 * 24;
+  const due = new Date(randomDueDate);
+
+  const owner = owners[Math.floor(Math.random() * owners.length)];
+  const status = statuses[Math.floor(Math.random() * statuses.length)];
+  const effort = Math.ceil(Math.random() * 20);
+  const title = `Lorem ipsum del iptenium, ${i}`;
+  const id = initialCount + i + 1;
+
+  const issue = {
+    id,
+    title,
+    created,
+    due,
+    owner,
+    status,
+    effort,
+  };
+
+  db.issues.insertOne(issue);
+}
+
+const count = db.issues.count();
+db.counters.update({ _id: 'issues' }, { $set: { current: count } });
+
+print('New issue count:', count);
